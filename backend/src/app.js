9da4ec762a183ac0d8a72d8c26789ac2fff52d37
sidd51 <siddhiborawake2004@gmail.com>
2026-02-10 00:06:10 +0530

    Add CORS support and integrate Axios for code review feature in frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,14 @@
 const express= require('express');
 const app=express()
 const aiRoutes = require("./routes/ai.routes");
+const cors=require('cors')
 
+app.use(cors())
 app.use(express.json())
 app.get('/',(req,res)=>{
   res.send('Hello World')
 })
+
 
 app.use('/ai',aiRoutes)
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,53 @@
-import { useState, useEffect } from 'react'
+import { useState } from "react";
+import Editor from "react-simple-code-editor";
 
+import Prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
+import "prismjs/components/prism-javascript";
+import Markdown from 'react-markdown'
+import axios from 'axios'
 import "./App.css";
-import "prismjs/themes/prism-tomorrow.css"
-import prism from "prismjs"
-function App() {
 
-  useEffect(()=>{
-    prism.highlightAll()
-  })
+function App() {
+  const [code, setCode] = useState(`function sum() {
+  return 1 + 1;
+  }`);
+  const [review, setReview]=useState(``)
+
+  async function reviewCode(){
+      const response= await axios.post('http://localhost:3000/ai/get-review',{ code });
+      setReview(response.data);
+  }
   return (
-    <main>
+    <main> 
       <div className="left">
         <div className="code">
-          <pre>
-            <code className='language-javascript'>
-              {`function sum(){
-                return 1 + 1
-              }`}
-            </code>
-          </pre>
+          <Editor
+            value={code}
+            onValueChange={(code) => setCode(code)}
+            highlight={(code) =>
+              Prism.highlight(code, Prism.languages.javascript, "javascript")
+            }
+            padding={10}
+            style={{
+              fontFamily: '"Fira Code", monospace',
+              fontSize: 14,
+              backgroundColor: "#1e1e1e",
+              color: "white",
+              minHeight: "100%",
+              borderRadius: "8px",
+            }}
+          />
         </div>
-        <div className="review">Review</div>
+
+        <div onClick={reviewCode} className="review">Review</div>
       </div>
 
-      <div className="right"></div>
+      <div className="right">
+         <Markdown>{review}</Markdown>
+      </div>
     </main>
   );
 }
- 
 
 export default App;
